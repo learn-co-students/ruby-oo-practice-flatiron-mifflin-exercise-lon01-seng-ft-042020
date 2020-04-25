53672e56feca99add45b9c4c2c986656a056392b
Juhagit <juha.ea.penttila@gmail.com>
2020-04-25 18:04:18 +0100

    Done.

--- run.rb
+++ run.rb
@@ -3,7 +3,16 @@ require_relative "lib/Employee"
 require 'pry'
 
 
-#Test your code here
+zac = Manager.new("Zac", "tech", 25)
+tony = Manager.new("Tony", "sales", 50)
+steve = Manager.new("Steve", "operations", 37)
+
+thor = Employee.new("Thor", 50000, zac)
+bruce = Employee.new("Bruce", 75000, zac)
+peter = Employee.new("Peter", 49500, tony)
+wanda = Employee.new("Wanda", 30000, tony)
+bucky = Employee.new("Bucky", 35000, steve)
+natasha = Employee.new("Natasha", 50100, steve)
 
 
 binding.pry
